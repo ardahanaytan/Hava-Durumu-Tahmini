fb3049b3af3ad0fbe54774a54955dc7f7aeafc98
ardahanaytan <135515876+ardahanaytan@users.noreply.github.com>
2024-08-23 16:31:23 +0300

    normalization & time series

--- data/data.py
+++ data/data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn import preprocessing
 
 df_basinc = pd.read_csv('data/weather_datas/basinc.csv')
 df_bulut = pd.read_csv('data/weather_datas/bulut.csv')
@@ -7,18 +8,28 @@ df_ruzgar = pd.read_csv('data/weather_datas/ruzgar_hizi_yonu.csv')
 df_sicaklik = pd.read_csv('data/weather_datas/sicaklik.csv')
 df_yagis = pd.read_csv('data/weather_datas/yagis_miktari.csv')
 
+ctable = ['Basinc','Bulut','Nem','Ruzgar Hizi','Ruzgar Yonu','Sicaklik','Yagis']
 tables = [df_nem, df_ruzgar, df_sicaklik, df_yagis]
 real_df = pd.merge(df_basinc, df_bulut, on = ['Yil', 'Ay', 'Gun', 'Saat'])
 
 for i in tables:
     real_df = pd.merge(real_df, i, on = ['Yil', 'Ay', 'Gun', 'Saat'])
 
-real_df.Yil = pd.Categorical(real_df.Yil)
-real_df.Ay = pd.Categorical(real_df.Ay)
-real_df.Gun = pd.Categorical(real_df.Gun)
-real_df.Saat = pd.Categorical(real_df.Saat)
-
 df = real_df.copy()
 df = df.dropna()
 
-print(df.tail())
+df_st = pd.DataFrame(preprocessing.scale(df.select_dtypes('float64')), columns=ctable)
+for i in ctable:
+    df[i] = df_st[i]
+
+df['Yil'] = df['Yil'].str[1:].astype(int)
+df['Ay'] = df['Ay'].map({
+    'Ocak': 1, 'Şubat': 2, 'Mart': 3, 'Nisan': 4, 'Mayıs': 5, 'Haziran': 6,
+    'Temmuz': 7, 'Ağustos': 8, 'Eylül': 9, 'Ekim': 10, 'Kasım': 11, 'Aralık': 12
+})
+df['Gun'] = df['Gun'].str[1:].astype(int)
+df['Saat'] = df['Saat'].str[1:].str[:2].astype(int)
+
+df['datetime'] = pd.to_datetime(df[['Yil', 'Ay', 'Gun']].astype(str).agg('-'.join, axis=1) + ' ' + df['Saat'].astype(str), format='%Y-%m-%d %H')
+df.set_index('datetime', inplace=True)
+df.drop(columns=['Yil', 'Ay', 'Gun', 'Saat'], inplace=True)
