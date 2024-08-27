af18ad56e1f1e714f85da09870d7664eb5859f22
ardahanaytan <135515876+ardahanaytan@users.noreply.github.com>
2024-08-27 16:03:36 +0300

    tried to improve the model

--- data/data.py
+++ data/data.py
@@ -1,15 +1,16 @@
 import pandas as pd
-from sklearn import preprocessing
+#from sklearn import preprocessing
 
 df_basinc = pd.read_csv('data/weather_datas/basinc.csv')
 df_bulut = pd.read_csv('data/weather_datas/bulut.csv')
 df_nem = pd.read_csv('data/weather_datas/nem.csv')
 df_ruzgar = pd.read_csv('data/weather_datas/ruzgar_hizi_yonu.csv')
 df_sicaklik = pd.read_csv('data/weather_datas/sicaklik.csv')
-df_yagis = pd.read_csv('data/weather_datas/yagis_miktari.csv')
+df_yagmur = pd.read_csv('data/weather_datas/yagmur.csv')
+df_kar = pd.read_csv('data/weather_datas/kar.csv')
 
-ctable = ['Basinc','Bulut','Nem','Ruzgar Hizi','Ruzgar Yonu','Sicaklik','Yagis']
-tables = [df_nem, df_ruzgar, df_sicaklik, df_yagis]
+ctable = ['Basinc','Bulut','Nem','Ruzgar Hizi','Ruzgar Yonu','Sicaklik','Yagis','Kar']
+tables = [df_nem, df_ruzgar, df_sicaklik, df_yagmur, df_kar]
 real_df = pd.merge(df_basinc, df_bulut, on = ['Yil', 'Ay', 'Gun', 'Saat'])
 
 for i in tables:
@@ -18,12 +19,7 @@ for i in tables:
 df = real_df.copy()
 df = df.dropna()
 
-df_mean = df['Basinc'].mean()
-df_std = df['Basinc'].std()
-
-df_st = pd.DataFrame(preprocessing.scale(df.select_dtypes('float64')), columns=ctable)
-for i in ctable:
-    df[i] = df_st[i]
+#df_st = pd.DataFrame(preprocessing.scale(df.select_dtypes('float64')), columns=ctable)
 
 df['Yil'] = df['Yil'].str[1:].astype(int)
 df['Ay'] = df['Ay'].map({
@@ -36,3 +32,4 @@ df['Saat'] = df['Saat'].str[1:].str[:2].astype(int)
 df['datetime'] = pd.to_datetime(df[['Yil', 'Ay', 'Gun']].astype(str).agg('-'.join, axis=1) + ' ' + df['Saat'].astype(str), format='%Y-%m-%d %H')
 df.set_index('datetime', inplace=True)
 df.drop(columns=['Yil', 'Ay', 'Gun', 'Saat'], inplace=True)
+df.columns = ctable
--- models/model.py
+++ models/model.py
@@ -2,26 +2,64 @@ import sys
 import pandas as pd
 sys.path.append('/Users/ardahanaytan/Desktop/STAJ/Hava Durumu Tahmin Projesi/data')
 from data import df
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
-from data import df_mean, df_std
+from sklearn.linear_model import Ridge
+rr = Ridge(alpha=0.1)
 
-X = df.drop('Basinc', axis = 1)
-y = df['Basinc']
+df['hedef'] = df.shift(-24)['Sicaklik']
+df.iloc[-24:] = df.iloc[-48:-24]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 270)
+predictors = df.columns[~df.columns.isin(['hedef'])]
 
-X_train = X_train.values.reshape((X_train.shape[0], 1, X_train.shape[1]))
-X_test = X_test.values.reshape((X_test.shape[0], 1, X_test.shape[1]))
+def backtest(weather, model, predictors, start = 40000, step = 24):
+    all_predictions = []
+    
+    for i in range(start, weather.shape[0], step):
+        train = weather.iloc[:i,:]
+        test = weather.iloc[i:(i+step),:]
+        
+        model.fit(train[predictors], train['hedef'])
+        
+        preds = model.predict(test[predictors])
+        
+        preds = pd.Series(preds, index=test.index)
+        combined = pd.concat([test['hedef'], preds], axis=1)
+        
+        combined.columns = ['actual','prediction']
+        combined['diff'] = (combined['prediction'] - combined['actual']).abs()
+        
+        all_predictions.append(combined)
+    return pd.concat(all_predictions)
 
-model = Sequential()
-model.add(LSTM(600, activation = 'relu', input_shape = (X_train.shape[1], X_train.shape[2])))
-model.add(Dense(1))
-model.compile(optimizer = 'adam', loss = 'mse')
+predictions = backtest(df, rr, predictors)
 
-model.fit(X_train, y_train, epochs=150, verbose=1)
+def pct_diff(old, new):
+    return (new - old) / old
 
-y_pred = model.predict(X_test)
-y_pred = pd.DataFrame(y_pred * df_std + df_mean)
-print(y_pred.head())
+def compute_rolling(weather, horizon, col):
+    label = f"rolling_{horizon}_{col}"
+    weather[label] = weather[col].rolling(horizon).mean()
+    weather[f"{label}_pct"] = pct_diff(weather[label], weather[col])
+    return weather
+
+rolling_horizons = [3,24]
+
+for horizon in rolling_horizons:
+    for col in ['Basinc','Bulut','Nem','Ruzgar Hizi','Ruzgar Yonu','Sicaklik','Yagis','Kar']:
+        df = compute_rolling(df, horizon, col)
+
+df = df.iloc[24:, :]
+df = df.fillna(0)
+
+def expand_mean(df):
+    return df.expanding(1).mean()
+
+for col in ['Basinc','Bulut','Nem','Ruzgar Hizi','Ruzgar Yonu','Sicaklik','Yagis','Kar']:
+    df[f'month_avg_{col}'] = df[col].groupby(df.index.month, group_keys=False).apply(expand_mean)
+    df[f'day_avg_{col}'] = df[col].groupby(df.index.day_of_year, group_keys=False).apply(expand_mean)
+    df[f'hour_avg_{col}'] = df[col].groupby(df.index.hour, group_keys=False).apply(expand_mean)
+
+predictors = df.columns[~df.columns.isin(['hedef'])]
+predictions = backtest(df, rr, predictors)
+print(predictions['diff'].mean())
+
+# SICAKLIK ICIN EN DUSUK -> 2.0638077871120877
