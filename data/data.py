cf8065cfc0dc56b4e46ec033495af2cbd5940238
ardahanaytan <135515876+ardahanaytan@users.noreply.github.com>
2024-09-02 16:50:00 +0300

    made a temp model and tried to not hurt myself

--- data/data.py
+++ data/data.py
@@ -3,31 +3,21 @@ import pandas as pd
 df_basinc = pd.read_csv('data/weather_datas/basinc.csv')
 df_bulut = pd.read_csv('data/weather_datas/bulut.csv')
 df_nem = pd.read_csv('data/weather_datas/nem.csv')
-df_ruzgar = pd.read_csv('data/weather_datas/ruzgar_hizi_yonu.csv')
+df_ruzgar = pd.read_csv('data/weather_datas/ruzgar.csv')
 df_sicaklik = pd.read_csv('data/weather_datas/sicaklik.csv')
 df_yagmur = pd.read_csv('data/weather_datas/yagmur.csv')
 df_kar = pd.read_csv('data/weather_datas/kar.csv')
 
-ctable = ['Basinc','Bulut','Nem','Ruzgar Hizi','Ruzgar Yonu','Sicaklik','Yagis','Kar']
 tables = [df_nem, df_ruzgar, df_sicaklik, df_yagmur, df_kar]
-real_df = pd.merge(df_basinc, df_bulut, on = ['Yil', 'Ay', 'Gun', 'Saat'])
-
+df = pd.merge(df_basinc, df_bulut, on=['Zaman'])
 for i in tables:
-    real_df = pd.merge(real_df, i, on = ['Yil', 'Ay', 'Gun', 'Saat'])
+    df = pd.merge(df, i, on=['Zaman'])
 
-df = real_df.copy()
-df = df.dropna()
-
-df['Yil'] = df['Yil'].str[1:].astype(int)
-df['Ay'] = df['Ay'].map({
-    'Ocak': 1, 'Şubat': 2, 'Mart': 3, 'Nisan': 4, 'Mayıs': 5, 'Haziran': 6,
-    'Temmuz': 7, 'Ağustos': 8, 'Eylül': 9, 'Ekim': 10, 'Kasım': 11, 'Aralık': 12
-})
-df['Gun'] = df['Gun'].str[1:].astype(int)
-df['Saat'] = df['Saat'].str[1:].str[:2].astype(int)
-
+df['Yil'] = df['Zaman'].str[:4].astype(int)
+df['Ay'] = df['Zaman'].str[4:6].astype(int)
+df['Gun'] = df['Zaman'].str[6:8].astype(int)
+df['Saat'] = df['Zaman'].str[9:].str[:2].astype(int)
 df['datetime'] = pd.to_datetime(df[['Yil', 'Ay', 'Gun']].astype(str).agg('-'.join, axis=1) + ' ' + 
                                 df['Saat'].astype(str), format='%Y-%m-%d %H')
 df.set_index('datetime', inplace=True)
-df.drop(columns=['Yil', 'Ay', 'Gun', 'Saat'], inplace=True)
-df.columns = ctable
+df.drop(columns=['Zaman', 'Yil', 'Ay', 'Gun', 'Saat'], inplace=True)
---
+++ models/model_ruzgar_hizi.py
@@ -0,0 +1,29 @@
+import sys
+import pandas as pd
+sys.path.append('/Users/ardahanaytan/Desktop/STAJ/Hava Durumu Tahmin Projesi/data')
+from data import df
+from sklearn.metrics import mean_squared_error
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.stattools import acf, pacf
+
+df_t = pd.DataFrame({'Ruzgar_Hizi': df['Ruzgar_Hizi']})
+train_df = df_t[:'2020'].resample('M').mean()
+test_df = df_t['2021':].resample('M').mean()
+
+acf_lag = acf(train_df.values, nlags=20)
+pacf_lag = pacf(train_df.values, nlags=20, method='ols')
+
+model = ARIMA(train_df.values, order=(2,0,2))
+model_fit = model.fit()
+
+forecast_obj = model_fit.get_forecast(steps=44)
+
+fc = forecast_obj.predicted_mean
+se = forecast_obj.se_mean
+conf = forecast_obj.conf_int(alpha=0.05)
+
+fc_series = pd.Series(fc, index=test_df.index)
+lower_series = pd.Series(conf[:, 0], index=test_df.index)
+upper_series = pd.Series(conf[:, 1], index=test_df.index)
+
+#Test Mean Squared Error:  1.5383022874796373  DAHA DA IYILESTIRILMELI, EN IYISI BU ARIMA ILE
