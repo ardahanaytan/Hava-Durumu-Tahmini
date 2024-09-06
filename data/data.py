c33f6d467a544533a7ee6c7d14d8dd60383a73f3
ardahanaytan <135515876+ardahanaytan@users.noreply.github.com>
2024-09-06 16:48:12 +0300

    just updated datas

--- data/data.py
+++ data/data.py
@@ -9,17 +9,17 @@ df_yagmur = pd.read_csv('data/weather_datas/yagmur.csv')
 df_kar = pd.read_csv('data/weather_datas/kar.csv')
 
 tables = [df_nem, df_ruzgar, df_sicaklik, df_yagmur, df_kar]
-df = pd.merge(df_basinc, df_bulut, on=['Zaman'])
+df = pd.merge(df_basinc, df_bulut, on=['location'])
 for i in tables:
-    df = pd.merge(df, i, on=['Zaman'])
+    df = pd.merge(df, i, on=['location'])
 
 df = df.dropna()
 
-df['Yil'] = df['Zaman'].str[:4].astype(int)
-df['Ay'] = df['Zaman'].str[4:6].astype(int)
-df['Gun'] = df['Zaman'].str[6:8].astype(int)
-df['Saat'] = df['Zaman'].str[9:].str[:2].astype(int)
+df['Yil'] = df['location'].str[:4].astype(int)
+df['Ay'] = df['location'].str[4:6].astype(int)
+df['Gun'] = df['location'].str[6:8].astype(int)
+df['Saat'] = df['location'].str[9:].str[:2].astype(int)
 df['datetime'] = pd.to_datetime(df[['Yil', 'Ay', 'Gun']].astype(str).agg('-'.join, axis=1) + ' ' + 
                                 df['Saat'].astype(str), format='%Y-%m-%d %H')
 df.set_index('datetime', inplace=True)
-df.drop(columns=['Zaman', 'Yil', 'Ay', 'Gun', 'Saat'], inplace=True)
+df.drop(columns=['location', 'Yil', 'Ay', 'Gun', 'Saat'], inplace=True)
