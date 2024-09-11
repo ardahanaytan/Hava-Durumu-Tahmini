9edd83692ac4e2d3707a3a4afab64730b12f8240
ardahanaytan <135515876+ardahanaytan@users.noreply.github.com>
2024-09-11 11:56:31 +0300

    added a navbar

--- web/app.py
+++ web/app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, send_from_directory
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
