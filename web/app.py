5876c92b4c229cac30fba7bf5710109cb930ca89
ardahanaytan <135515876+ardahanaytan@users.noreply.github.com>
2024-09-13 15:47:18 +0300

    created all websites and finished mostly

--- web/app.py
+++ web/app.py
@@ -5,5 +5,37 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/amsterdam')
+def amsterdam():
+    return render_template('amsterdam.html')
+
+@app.route('/berlin')
+def berlin():
+    return render_template('berlin.html')
+
+@app.route('/istanbul')
+def istanbul():
+    return render_template('istanbul.html')
+
+@app.route('/londra')
+def londra():
+    return render_template('londra.html')
+
+@app.route('/madrid')
+def madrid():
+    return render_template('madrid.html')
+
+@app.route('/paris')
+def paris():
+    return render_template('paris.html')
+
+@app.route('/roma')
+def roma():
+    return render_template('roma.html')
+
+@app.route('/raporlar')
+def raporlar():
+    return render_template('raporlar.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
